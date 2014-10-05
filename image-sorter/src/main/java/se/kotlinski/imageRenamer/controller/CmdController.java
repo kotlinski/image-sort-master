5e8c9c31579236f209ef10dc4a6f8a39f2f97c54
Simon Kotlinski <simon.kotlinski@gmail.com>
2014-10-05 23:35:49 +0200

    Updated readme, moved packages

---
+++ image-sorter/src/main/java/se/kotlinski/imageRenamer/controller/CmdController.java
@@ -0,0 +1,38 @@
+package se.kotlinski.imageRenamer.controller;
+
+import org.apache.commons.cli.CommandLine;
+import se.kotlinski.imageRenamer.model.FolderIO;
+import se.kotlinski.imageRenamer.utils.CommandLineUtil;
+import se.kotlinski.imageRenamer.utils.ImageIndex;
+
+/**
+ * Run the Image Renamer via command-line.
+ *
+ * @author Simon Kotlinski
+ * @version $Revision: 1.1 $
+ */
+public class CmdController {
+	private FolderIO folderIO;
+	private Object imageMapper;
+
+	public CmdController() {
+	}
+	public Object getImageMapper() {
+		return imageMapper;
+	}
+
+	public FolderIO getFolderIO() {
+		return folderIO;
+	}
+
+	public void startCmd(String[] argv) {
+		CommandLine cmd = CommandLineUtil.intepreterArgs(argv);
+		folderIO = CommandLineUtil.runCmd(CommandLineUtil.getOptions(), cmd);
+
+		ImageIndex imageIndex = new ImageIndex(folderIO);
+
+		System.out.println(folderIO);
+		imageMapper = imageIndex.runIndexing();
+		//System.out.println(imageMapper);
+	}
+}
---
+++ image-sorter/src/main/java/se/kotlinski/imageRenamer/mapper/ImageMapper.java
@@ -0,0 +1,98 @@
+package se.kotlinski.imageRenamer.mapper;
+
+import se.kotlinski.imageRenamer.model.ImageDescriber;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+
+/**
+ * Created by Simon Kotlinski on 2014-05-07.
+ */
+public class ImageMapper {
+	HashMap<String, ArrayList<ImageDescriber>> imageMap;
+
+	public ImageMapper() {
+		imageMap = new HashMap<String, ArrayList<ImageDescriber>>();
+	}
+
+	public static ArrayList<ImageDescriber> recursiveIterate(final File folder) {
+		ArrayList<ImageDescriber> imageDescriber = new ArrayList<ImageDescriber>();
+		for (File file : folder.listFiles()) {
+			if (file.isDirectory()) {
+				imageDescriber.addAll(recursiveIterate(file));
+			}
+			else {
+				imageDescriber.add(new ImageDescriber(file));
+			}
+		}
+		return imageDescriber;
+	}
+
+	public void addImage(ImageDescriber imageDescriber) {
+		String md5 = imageDescriber.getMd5();
+		ArrayList<ImageDescriber> imageArray = getImageArrayForMd5(imageDescriber, md5);
+		imageArray.add(imageDescriber);
+	}
+
+	private ArrayList<ImageDescriber> getImageArrayForMd5(ImageDescriber imageDescriber, String md5) {
+		ArrayList<ImageDescriber> imageArray;
+		if (imageMap.containsKey(md5)) {
+			imageArray = imageMap.get(md5);
+		}
+		else {
+			imageArray = new ArrayList<ImageDescriber>();
+			imageMap.put(md5, imageArray);
+		}
+		return imageArray;
+	}
+
+	public void populateWithImages(ArrayList<File> inputFolders) {
+		ArrayList<ImageDescriber> images = new ArrayList<ImageDescriber>();
+		for (File rootFolder : inputFolders) {
+			images.addAll(recursiveIterate(rootFolder));
+		}
+		for (ImageDescriber image : images) {
+			addImage(image);
+		}
+	}
+
+
+	public ArrayList<ImageDescriber> getUniqueImageDescribers() {
+		ArrayList<ImageDescriber> imageDescribers = new ArrayList<ImageDescriber>();
+		for (String s : imageMap.keySet()) {
+			imageDescribers.add(imageMap.get(s).get(0));
+		}
+		imageDescribers.sort(Comparator.<ImageDescriber>naturalOrder());
+		return imageDescribers;
+	}
+
+	public int getSizeOfUniqueImages() {
+		return imageMap.size();
+	}
+
+	@Override
+	public String toString() {
+		String retString = "Files in input folder: \n";
+		for (String s : imageMap.keySet()) {
+			retString += s + ", including files: " + "\n";
+			for (ImageDescriber imageDescriber : imageMap.get(s)) {
+				retString += "\t" + imageDescriber.toString() + "\n";
+			}
+		}
+		return retString;
+	}
+
+
+	public ArrayList<ImageDescriber> getRedundantFiles() {
+		ArrayList<ImageDescriber> imageDescribers = new ArrayList<ImageDescriber>();
+		for (String s : imageMap.keySet()) {
+				for (int i = 1; i < imageMap.get(s).size(); i++) {
+				imageDescribers.add(imageMap.get(s).get(i));
+			}
+		}
+		imageDescribers.sort(Comparator.<ImageDescriber>naturalOrder());
+		return imageDescribers;
+	}
+}
