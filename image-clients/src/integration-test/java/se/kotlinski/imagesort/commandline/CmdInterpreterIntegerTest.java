057248a110d3cebd98f81e1f3f00824f8553b1e8
Simon Kotlinski <simon.kotlinski@gmail.com>
2015-09-26 01:37:39 +0200

    Added integration test config in image sorter project

---
+++ image-clients/src/integration-test/java/se/kotlinski/imagesort/commandline/CmdInterpreterIntegerTest.java
@@ -0,0 +1,48 @@
+package se.kotlinski.imagesort.commandline;
+
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import se.kotlinski.imagesort.commandline.argument.Interpreter;
+import se.kotlinski.imagesort.commandline.argument.Transformer;
+import se.kotlinski.imagesort.exception.InvalidFolderArgumentsException;
+import se.kotlinski.imagesort.utils.SortMasterFileUtil;
+
+import static org.mockito.Mockito.mock;
+
+public class CmdInterpreterIntegerTest {
+
+	private Interpreter cmdInterpreter;
+	private SortMasterFileUtil sortMasterFileUtil;
+
+	@Before
+	public void setUp() {
+
+		//Mock this up instead of running the real thing?
+		sortMasterFileUtil = new SortMasterFileUtil();
+		CommandLineParser parser = new GnuParser();
+		HelpFormatter formatter = mock(HelpFormatter.class);
+		Transformer transformer = new Transformer(formatter, parser, sortMasterFileUtil);
+
+		cmdInterpreter = new Interpreter(transformer);
+	}
+
+
+	@Test (expected = InvalidFolderArgumentsException.class)
+	public void testGetFolderInputInvalidIntegration() throws Exception {
+		String[] arguments = new String[]{"programName", "someCommand", "-s", "", "-o", sortMasterFileUtil
+				.getTestOutputPath()};
+		cmdInterpreter.getFolderIO(arguments);
+	}
+
+	@Test (expected = InvalidFolderArgumentsException.class)
+	public void testGetFolderInputNull() throws Exception {
+		String[] arguments = new String[]{"programName", "someCommand", "-s", null, "-o",
+				sortMasterFileUtil.getTestOutputPath()};
+		cmdInterpreter.getFolderIO(arguments);
+	}
+
+}
